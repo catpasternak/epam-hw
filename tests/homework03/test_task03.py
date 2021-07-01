961dc9172ffca283f2add275ba2e4535c78a0a1a
Ekaterina Pasternak <catpasternak@gmail.com>
2021-07-02 00:31:26 +0300

    Homework 3, checks fixed

--- tests/homework03/test_task03.py
+++ tests/homework03/test_task03.py
@@ -11,7 +11,7 @@ def test_filter_object_created_with_function_delivers_correctly():
     assert make_filter(name="Jane", age=5).apply(sample_data) == [
         {"name": "Jane", "age": 5, "color": "red"}
     ]
- 
+
 
 def test_filter_returns_empty_when_one_parameter_is_not_present_in_source_data():
     """Testing that filter object with its method apply() returns only appropriate elements of given data"""
@@ -21,4 +21,3 @@ def test_filter_returns_empty_when_one_parameter_is_not_present_in_source_data()
     ]
 
     assert make_filter(name="Jane", age=100).apply(sample_data) == []
-    
