2b028c7c5c3879b4165d7fd115ebda60b0ec7de1
Ekaterina Pasternak <catpasternak@gmail.com>
2021-07-01 15:18:36 +0300

    Homework 2, fixed 2nd check failure

--- tests/homework02/test_task01.py
+++ tests/homework02/test_task01.py
@@ -1,12 +1,6 @@
 import os
 
-from homework02.task01 import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+from homework02 import task01
 
 
 def _path_to(filename):
@@ -15,7 +9,7 @@ def _path_to(filename):
 
 def test_longest_diverse_words():
     """Testing that function returns 10 words with biggest amount of unique symbols in it"""
-    assert get_longest_diverse_words(_path_to("sample01_task01.txt")) == [
+    assert task01.get_longest_diverse_words(_path_to("sample01_task01.txt")) == [
         "vorzutäuschen",
         "Fruchtbarkeit",
         "herabgesunken",
@@ -31,19 +25,19 @@ def test_longest_diverse_words():
 
 def test_rarest_char():
     """Testing that least common char in given file is returned"""
-    assert get_rarest_char(_path_to("sample01_task01.txt")) == "j"
+    assert task01.get_rarest_char(_path_to("sample01_task01.txt")) == "j"
 
 
 def test_punctuation_chars_count():
     """Testing that function counts every punctuation char in file"""
-    assert count_punctuation_chars(_path_to("sample01_task01.txt")) == 12
+    assert task01.count_punctuation_chars(_path_to("sample01_task01.txt")) == 12
 
 
 def test_non_ascii_chars_count():
     """Testing that function counts every non-ASCII char in given file"""
-    assert count_non_ascii_chars(_path_to("sample01_task01.txt")) == 7
+    assert task01.count_non_ascii_chars(_path_to("sample01_task01.txt")) == 7
 
 
 def test_most_common_non_ascii_char():
     """Testing that function returns most common non-ASCII char from file"""
-    assert get_most_common_non_ascii_char(_path_to("sample01_task01.txt")) == "ä"
+    assert task01.get_most_common_non_ascii_char(_path_to("sample01_task01.txt")) == "ä"
