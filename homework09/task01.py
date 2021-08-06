81900a99753df756d329323e195659f67e82bc51
Ekaterina Pasternak <catpasternak@gmail.com>
2021-08-06 11:53:14 +0300

    Homework 9, minor improvements

--- homework09/task01.py
+++ homework09/task01.py
@@ -13,50 +13,6 @@ file2.txt:
 """
 
 
-def yield_from_file(path_to_file):
-    """Generates integers from file.
-    :param path_to_file: path to file that contains only integers, one per line
-    :type path_to_file: str
-    :return: yields integers one by one
-    :rtype: Genarator
-    """
-    with open(path_to_file) as file_input:
-        for line in file_input:
-            yield int(line.rstrip())
-
-
-def create_list_of_incoming_gens(file_list):
-    """Creates list that contains generators that generate integers from given files.
-    :param file_list: list of files with integers sorted from min to max
-    :type file_list: List[str]
-    :return: list of generators created from files from list of files
-    :rtype: List[Generator]
-    """
-    incoming_gens = []
-    for file_name in file_list:
-        incoming_gen = yield_from_file(file_name)
-        incoming_gens.append(incoming_gen)
-    return incoming_gens
-
-
-def find_next_to_yield(incoming_values):
-    """Finds minimun value in list. There might be None values in list.
-    :param incoming_values: list of next values from list of generators
-    :type incoming_values: List[int, None]
-    :return: minimum value among values
-    :rtype: int
-    """
-    for value in incoming_values:
-        if value is not None:
-            next_to_yield = value
-            break
-    for value in incoming_values:
-        if value is not None:
-            if value < next_to_yield:
-                next_to_yield = value
-    return next_to_yield
-
-
 def merge_sorted_files(file_list):
     """Generates sequence that contains integers from files in ascending order.
     - Creates list of generators, one from each file;
@@ -85,3 +41,47 @@ def merge_sorted_files(file_list):
         next_to_yield = find_next_to_yield(incoming_values)
         yield next_to_yield
         index_list = [incoming_values.index(next_to_yield)]
+
+
+def create_list_of_incoming_gens(file_list):
+    """Creates list that contains generators that generate integers from given files.
+    :param file_list: list of files with integers sorted from min to max
+    :type file_list: List[str]
+    :return: list of generators created from files from list of files
+    :rtype: List[Generator]
+    """
+    incoming_gens = []
+    for file_name in file_list:
+        incoming_gen = yield_from_file(file_name)
+        incoming_gens.append(incoming_gen)
+    return incoming_gens
+
+
+def yield_from_file(path_to_file):
+    """Generates integers from file.
+    :param path_to_file: path to file that contains only integers, one per line
+    :type path_to_file: str
+    :return: yields integers one by one
+    :rtype: Genarator
+    """
+    with open(path_to_file) as file_input:
+        for line in file_input:
+            yield int(line.rstrip())
+
+
+def find_next_to_yield(incoming_values):
+    """Finds minimun value in list. There might be None values in list.
+    :param incoming_values: list of next values from list of generators
+    :type incoming_values: List[int, None]
+    :return: minimum value among values
+    :rtype: int
+    """
+    for value in incoming_values:
+        if value is not None:
+            next_to_yield = value
+            break
+    for value in incoming_values:
+        if value is not None:
+            if value < next_to_yield:
+                next_to_yield = value
+    return next_to_yield
--- homework09/task03.py
+++ homework09/task03.py
@@ -12,17 +12,28 @@ import os
 import re
 
 
-def tokenize_by_lines(file_input):
-    """Generator that yields lines from file."""
+def tokenize_with_option(file_input, tokenizer):
+    """Generator that yields lines from file or tokens from lines
+    if tokenizer is not None.
+    :param file_input: content of file
+    :type: :class:`TextIOWrapper`
+    :param tokenizer: tokenizer function
+    :type tokenizer: Callable
+    :return: yields lines or tokens from lines
+    :rtype: str
+    """
     while line := file_input.readline():
-        yield line
-        line = ""
+        if not tokenizer:
+            yield line
+        else:
+            yield from tokenizer(line)
 
 
 def universal_file_counter(dir_path, file_extension, tokenizer=None):
     """Counts number of lines in files in given directory with given extension.
     If tokenizer parameter not None - will count tokens instead of lines.
     - Gets files with given extension;
+    - Counts lines or tokens in file.
     :param dir_path: Path to directory with files
     :type path: Path
     :param file_extension: Extension of files that function takes into accout
@@ -33,18 +44,13 @@ def universal_file_counter(dir_path, file_extension, tokenizer=None):
     :rtype: int
     """
     counter = 0
-    re_extension = "." + file_extension + "$"
     filenames = (
         os.path.join(dir_path, fi)
         for fi in os.listdir(dir_path)
-        if re.search(re_extension, fi)
+        if fi.endswith(file_extension)
     )
     for fi in filenames:
         with open(fi) as file_input:
-            for line in tokenize_by_lines(file_input):
-                if tokenizer:
-                    for token in tokenizer(line):
-                        counter += 1
-                else:
-                    counter += 1
+            for token in tokenize_with_option(file_input, tokenizer):
+                counter += 1
     return counter
